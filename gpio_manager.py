c3d8ef4d42bd5abedab94a902a0be3702b791d41
grro <gregor.roth@1und1.de>
2024-11-14 08:23:23 +0100

    initial check in

--- gpio_manager.py
+++ gpio_manager.py
@@ -1,6 +1,5 @@
 import logging
 import gpiod
-from gpiod.line import Direction, Value
 
 
 
@@ -11,9 +10,12 @@ class LedDevice:
         self.name = name
         self.gpio_number = gpio_number
         self.is_on = False
-        self.chip=gpiod.Chip('/dev/gpiochip0')
-        self.lines = self.chip.get_lines([ self.gpio_number ])
-        self.lines.request(consumer='gpio', type=gpiod.LINE_REQ_DIR_OUT)
+        chip = gpiod.Chip('/dev/gpiochip0')
+        self.led = chip.get_line(self.gpio_number)
+        config = gpiod.line_request()
+        config.consumer = "Blink"
+        config.request_type = gpiod.line_request.DIRECTION_OUTPUT
+        self.led.request(config)
         self.__listener = lambda: None    # "empty" listener
 
     def set_listener(self,listener):
@@ -27,6 +29,6 @@ class LedDevice:
 
     def switch(self, on: bool):
         logging.info("set LED to " + str(on))
-        self.lines.set_values([ 1 if on else 0 ])
+        self.led.set_value(1 if on else 0)
         self.is_on = True
         self.__notify_listener()
