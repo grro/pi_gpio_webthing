033d271eb529081e0c22e3f9dc03baa3b35dd880
grro <gregor.roth@1und1.de>
2025-06-21 09:10:11 +0200

    GPIO In support added

--- gpio_manager.py
+++ gpio_manager.py
@@ -53,11 +53,10 @@ class InGpio:
         self.listener = listener
 
     def __check(self):
-        logging.info(GPIO.input(self.gpio_number))
         new_on = GPIO.input(self.gpio_number) == 1
         if new_on != self.__on:
             self.__on = new_on
-            logging.info("GPIO IN " + self.name + " new state " + str(self.on))
+            logging.info("GPIO IN " + self.name + " new state " + str(self.on) + " (GPIO: " + str(GPIO.input(self.gpio_number)) + ")")
             self.listener()
 
     def __loop(self):
